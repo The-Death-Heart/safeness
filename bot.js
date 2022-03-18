95a79f0926aa190bf082e0f9331188930a1b824e
= <=>
2022-03-18 16:58:01 -0300

    Updated main bot file

---
+++ bot.js
@@ -0,0 +1,84 @@
+const { Collection, Client, MessageEmbed, MessageActionRow, MessageButton, Message } = require("discord.js");
+const db = require("./db");
+const logs = require("./logs");
+const fs = require("fs");
+const client = new Client({
+    intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]
+});
+
+client.commands = new Collection();
+
+const commandsDir = fs.readdirSync("commands").filter(f => f.endsWith(".js"));
+let loadedCommands = 0;
+for (const f of commandsDir) {
+    try {
+        const command = require(`./commands/${f}`);
+        if (!command.name) {
+            logs.error("bot", `Command file ${f} has no name property, so it won't be loaded`);
+            continue;
+        }
+        if (!command.execute) {
+            logs.error("bot", `Command file ${f} has no execute property, so it won't be loaded`);
+            continue;
+        }
+        if (!command.aliases) logs.warn("bot", `Command file ${f} has no aliases property`);
+        client.commands.set(command.name, command);
+        loadedCommands += 1;
+    }
+    catch (err) {
+        logs.error('bot', `Couldn't load command file ${f} due to an unknown error\n${err.stack}`);
+    }
+}
+
+client.on("ready", async () => {
+    logs.success('bot', 'Successfully connected to discord');
+});
+
+client.on("message", async message => {
+    if (!message.guild || message.author.bot) return;
+    /**
+     * @returns {Promise<Message>}
+     */
+    async function getInput() {
+        const filter = m => m.author.id === message.author.id;
+        const collected = await message.channel.awaitMessages({ filter, max: 1 });
+        return collected.first();
+    }
+    /**
+     * @param {string | object} content
+     * @returns {Promise<Message>}
+     */
+    async function reply(content) {
+        if (typeof content === "object") {
+            content.allowedMentions = { repliedUser: false };
+        }
+        else if (typeof content === "string") {
+            content = { content, allowedMentions: { repliedUser: false } }
+        }
+        return await message.reply(content).catch(err => {
+            logs.error("bot", err.stack);
+            content = content.content;
+            message.channel.send(content);
+        });
+    }
+    const prefix = ".";
+    if (!message.content.startsWith(prefix)) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if (cmd === "" || cmd === " ") return;
+    const foundCmd = client.commands.get(cmd) ?? client.commands.find(c => {
+        if (c.aliases) {
+            if (c.aliases.includes(cmd)) return true;
+            else return false;
+        }
+        else return false;
+    });
+    if (!foundCmd) return reply(`Comando no encontrado - command not found`);
+    try {
+        foundCmd.execute(message, args, reply, getInput);
+    }
+    catch (err) {
+        logs.error(`Error while executing command ${foundCmd.name}\n${err.stack}`);
+        reply("An unexpected error ocurred");
+    }
+});
