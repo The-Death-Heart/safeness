e7dcb8c5f7af1f9e0ef3549fcf1709e454511009
= <=>
2022-03-18 16:57:55 -0300

    Updated db.js

---
+++ db.js
@@ -0,0 +1,7 @@
+const { createConnection } = require("mysql");
+const data = require("./data");
+const db = createConnection(data.database);
+db.connect();
+db.query = require("util").promisify(db.query);
+
+module.exports = db;
