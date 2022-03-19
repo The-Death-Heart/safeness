e47c17adcefc42368cbe3d9448e942113c30f4f1
santiadjmc <santiadjmc@gmail.com>
2022-03-19 13:07:31 -0300

    Also fixed error

--- commands/setlang.js
+++ commands/setlang.js
@@ -13,7 +13,7 @@ module.exports = {
      */
     execute: async function (message, args, reply, getInput) {
         let foundL = await db.query("SELECT * FROM langs WHERE langs.id = ?", [message.author.id]);
-        let lang = foundL[0] ? foundL[0] : "es";
+        let lang = foundL[0] ? foundL[0].lang : "es";
         const responses = {
             alreadySet: {
                 es: "Ya has establecido ese idioma",
@@ -40,9 +40,9 @@ module.exports = {
         const targetLang = args[0];
         if (!targetLang) return reply(responses.noLang[lang]);
         if (targetLang.length !== 2) return reply(responses.lenghtError[lang]);
-        if (!lang.some(l => l === targetLang)) return reply(responses.invalidLang[lang]);
+        if (!langs.some(l => l === targetLang)) return reply(responses.invalidLang[lang]);
         if (!foundL[0]) {
-            await db.query("INSERT INTO langs SET ?", [{ id: message.activity.author.id, lang: targetLang }]);
+            await db.query("INSERT INTO langs SET ?", [{ id: message.author.id, lang: targetLang }]);
         }
         else {
             await db.query("UPDATE langs SET langs.lang = ? WHERE langs.id = ?", [targetLang, message.author.id]);
