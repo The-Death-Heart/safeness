57ee455150c1b6683b6dd3e87aadb5702ad85010
santiadjmc <santiadjmc@gmail.com>
2022-03-18 22:07:49 -0300

    Moved data.js and db.js

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 const { createConnection } = require("mysql");
-const data = require("./data");
+const data = require("../data/data");
 const db = createConnection(data.database);
 db.connect();
 db.query = require("util").promisify(db.query);
