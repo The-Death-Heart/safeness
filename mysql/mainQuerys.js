2c54f536e76654d5b3b516bd6a30ec0c574bd1cf
santiadjmc <santiadjmc@gmail.com>
2022-03-19 22:16:08 -0300

    Updated mainQuerys

--- mysql/mainQuerys.js
+++ mysql/mainQuerys.js
@@ -2,3 +2,4 @@ const data = require("../data/data");
 const db = require("../database/db");
 db.query("CREATE TABLE IF NOT EXISTS langs (id TEXT NOT NULL, lang VARCHAR(2) NOT NULL DEFAULT 'es')");
 db.query("CREATE TABLE IF NOT EXISTS prefixes (guildId TEXT NOT NULL, prefix VARCHAR(4) NOT NULL DEFAULT ?)", [data.defaultPrefix]);
+db.query("CREATE TABLE IF NOT EXISTS warnings (userId TEXT NOT NULL, guildId TEXT NOT NULL, reason TEXT NOT NULL DEFAULT 'No reason')");
