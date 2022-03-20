92e73b4458cd22bfbd8ada53a7f480ad7aada91b
santiadjmc <santiadjmc@gmail.com>
2022-03-20 17:35:28 -0300

    Added ranks to data

--- data/data.js
+++ data/data.js
@@ -6,6 +6,28 @@ const data = {
         database: process.env.DB_NAME
     },
     token: process.env.TOKEN,
-    defaultPrefix: "s!"
+    defaultPrefix: "s!",
+    ranks: {
+        1: {
+            name: "Soporte",
+            role_id: "955199686480896030"
+        },
+        2: {
+            name: "Moderador",
+            role_id: "949639525162954792"
+        },
+        3: {
+            name: "Administrador",
+            role_id: "949639159151218759"
+        },
+        4: {
+            name: "Director",
+            role_id: "950725408763047958"
+        },
+        5: {
+            name: "Fundador",
+            role_id: "949636926745509929"
+        }
+    }
 }
 module.exports = data;
