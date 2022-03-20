c40b75435806128124c70a8819343ee88e683c7e
santiadjmc <santiadjmc@gmail.com>
2022-03-20 17:35:51 -0300

    Updated ranks query

--- mysql/mainQuerys.js
+++ mysql/mainQuerys.js
@@ -4,3 +4,4 @@ db.query("CREATE TABLE IF NOT EXISTS langs (id TEXT NOT NULL, lang VARCHAR(2) NO
 db.query("CREATE TABLE IF NOT EXISTS prefixes (guildId TEXT NOT NULL, prefix VARCHAR(4) NOT NULL DEFAULT ?)", [data.defaultPrefix]);
 db.query("CREATE TABLE IF NOT EXISTS warnings (id INT(200) NOT NULL AUTO_INCREMENT, userId TEXT NOT NULL, guildId TEXT NOT NULL, reason TEXT NOT NULL, modId TEXT NOT NULL, PRIMARY KEY (id))");
 db.query("CREATE TABLE IF NOT EXISTS blacklist (userId TEXT NOT NULL)");
+db.query("CREATE TABLE IF NOT EXISTS staffs (id TEXT NOT NULL, rank INT NOT NULL DEFAULT 1)");
